82651b9467021bd7999f4189d2e60ad0d76c256a
Num | Developer | IT <70431034+SMNBDEIT@users.noreply.github.com>
2026-05-05 12:30:29 +0200

    Delete routes directory

--- routes/chatbotRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getResponse } = require('../controllers/chatbotController');
-const hanafiGuard = require('../middleware/hanafiGuard');
-
-router.post('/', hanafiGuard, getResponse);
-
-module.exports = router;
